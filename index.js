88097624c3b0c0a4ff3d85ecd53f48fb4c117e65
MincheolC <mccha0407@gmail.com>
2021-05-22 02:57:56 +0900

    allow cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Setup basic express server
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
@@ -9,6 +10,7 @@ server.listen(port, () => {
   console.log('Server listening at port %d', port);
 });
 
+app.use(cors());
 app.get('/', (req, res) => res.send('Server is running'));
 
 // Chatroom
