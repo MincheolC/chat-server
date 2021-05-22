323033666a9fa700e715611ab0a17ba0d504e401
MincheolC <mccha0407@gmail.com>
2021-05-22 14:01:47 +0900

    parse words and convert them to singular noun

---
+++ helper/__tests__/util.test.js
@@ -0,0 +1,83 @@
+const { parseWords, singularize, getSingularizedWords } = require('../util');
+
+describe('getWords', () => {
+  it('valid', () => {
+    const strings = [
+      'hello world',
+      'You can ask anything about Microsoft Cloud - Azure, Microsoft 365.',
+      'We provided a "special" gift - digital coupons - for you guys!',
+      'How (are) u?😀',
+      'print();',
+      '1 + 2 * 4 / 5 % 2 - 1 = ?'
+    ];
+    const result = [
+      ['hello', 'world'],
+      ['You', 'can', 'ask', 'anything', 'about', 'Microsoft', 'Cloud', 'Azure', 'Microsoft', '365'],
+      ['We', 'provided', 'a', 'special', 'gift', 'digital', 'coupons', 'for', 'you', 'guys'],
+      ['How', 'are', 'u'],
+      ['print'],
+      ['1', '2', '4', '5', '2', '1'],
+    ];
+    strings.forEach((str, index) => {
+      expect(parseWords(str)).toEqual(result[index]);
+    });
+  })
+});
+
+describe('singularize', () => {
+  it('valid', () => {
+    const plurals = [
+      'singles',
+      'applications',
+      'words',
+      'FMS',
+      'Microsoft',
+      'Azure',
+      '365',
+      'potatoes',
+      'analyses',
+      'sheep',
+      'house',
+    ];
+    const result = [
+      'single',
+      'application',
+      'word',
+      'FMS',
+      'Microsoft',
+      'Azure',
+      '365',
+      'potato',
+      'analysis',
+      'sheep',
+      'house',
+    ];
+    plurals.forEach((plural, index) => {
+      expect(singularize(plural)).toEqual(result[index]);
+    })
+  })
+});
+
+describe('getSingularizedWords', () => {
+  it('valid', () => {
+    const strings = [
+      'hello world',
+      'You can ask anything about Microsoft Cloud - Azure, Microsoft 365.',
+      'We provided "special" gifts - digital coupons - for you guys!',
+      'How (are) you?😀',
+      'print();',
+      '1 + 2 * 4 / 5 % 2 - 1 = ?',
+    ];
+    const result = [
+      ['hello', 'world'],
+      ['You', 'can', 'ask', 'anything', 'about', 'Microsoft', 'Cloud', 'Azure', 'Microsoft', '365'],
+      ['We', 'provided', 'special', 'gift', 'digital', 'coupon', 'for', 'you', 'guy'],
+      ['How', 'is', 'you'],
+      ['print'],
+      ['1', '2', '4', '5', '2', '1'],
+    ];
+    strings.forEach((str, index) => {
+      expect(getSingularizedWords(str)).toEqual(result[index]);
+    });
+  });
+});
--- helper/util.js
+++ helper/util.js
@@ -1,1 +1,21 @@
-// parser
+const pluralize = require('pluralize');
+
+function parseWords(str) {
+  const regexWords = /([a-zA-Z0-9]+)/g
+  return Array.from(str.matchAll(regexWords)).map(obj => obj[0]);
+}
+
+function singularize(word) {
+  const regexProperNoun = /^[A-Z0-9]+/
+  return regexProperNoun.test(word) ? word : pluralize.singular(word);
+}
+
+function getSingularizedWords(str) {
+  return parseWords(str).map(word => singularize(word));
+}
+
+module.exports = {
+  parseWords,
+  singularize,
+  getSingularizedWords,
+};
